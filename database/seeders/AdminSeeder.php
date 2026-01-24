7c31bbc7d3e3f5a9102a97c3ecb2cf69fc7ea2c5
Raden-Salaf <radensalaf28@gmail.com>
2026-01-25 00:18:15 +0700

    tambah admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->updateOrInsert(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('admin123'),
+                'role' => 'Admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
+    }
+}
